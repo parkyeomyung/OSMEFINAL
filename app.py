098422c9ad03ea520d1b43fcadab33ad50d93887
parkyeomyung <148339924+parkyeomyung@users.noreply.github.com>
2023-12-01 13:26:57 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,29 @@
 from flask import Flask, request, jsonify
 import openai
+import boto3
+import json
 
 app = Flask(__name__)
 
 def setup_openai_api(api_key):
     openai.api_key = api_key
+
+def get_openai_api_key():
+    # Secret Manager 클라이언트 생성
+    client = boto3.client('secretsmanager', region_name='ap-northeast-2')
+
+    # 시크릿 검색
+    response = client.get_secret_value(SecretId='OSME-vY5bd8')
+
+    # 시크릿 값 추출
+    if 'SecretString' in response:
+        secret = response['SecretString']
+    else:
+        secret = response['SecretBinary']
+
+    # 시크릿 파싱 (JSON 형식인 경우)
+    parsed_secret = json.loads(secret)
+    return parsed_secret['OSME']  # JSON 내의 실제 키 이름으로 변경
 
 def get_perfume_recommendation(adjective):
     prompt = f"향수 추천가로서, '{adjective}'이(가) 주요 특징인 향수를 1개 추천해주세요. 향수 이름과 그 특징을 설명해주세요."
@@ -38,7 +57,6 @@ def index():
     return app.send_static_file('index.html')
 
 if __name__ == "__main__":
-    api_key = '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'  # 실제 API 키로 교체하세요.
+    api_key = get_openai_api_key()  # AWS Secret Manager에서 API 키를 검색
     setup_openai_api(api_key)
     app.run(host='0.0.0.0', port=5000)
-        
